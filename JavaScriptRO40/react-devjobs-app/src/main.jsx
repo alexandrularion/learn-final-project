a25606eba5784924d0633ff47966b3929c251faf
alexandrularion <larion.a@yahoo.com>
2024-08-31 01:28:51 +0300

    Fetch data using browser server side loader

--- JavaScriptRO40/react-devjobs-app/src/main.jsx
+++ JavaScriptRO40/react-devjobs-app/src/main.jsx
@@ -4,16 +4,26 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import JobsPage from "./pages/jobs-page/jobs-page";
 import JobPage from "./pages/job-page/job-page";
+import jobService from "./server/job-service";
 
 // Components -> Pages -> main.jsx
 const router = createBrowserRouter([
   {
     path: "/",
     element: <JobsPage />,
+    loader: async () => {
+      const jobs = await jobService.readByFilters();
+      return jobs || null;
+    },
   },
   {
     path: "/jobs/:jobId",
     element: <JobPage />,
+    loader: async ({ params }) => {
+      const { jobId } = params;
+      const job = await jobService.readById(jobId);
+      return job || null;
+    },
   },
 ]);
 
