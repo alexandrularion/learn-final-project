b19a8284e15bd4bcc83f5401b628c115e6f52c2a
alexandrularion <larion.a@yahoo.com>
2024-12-03 21:19:12 +0200

    Added CSS variables + Platforms SVG's + Implemented Heading component

--- JavaScriptRO43/link-sharing-app/src/index.jsx
+++ JavaScriptRO43/link-sharing-app/src/index.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Heading } from "./common";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <>Hello world</>
+    <Heading>Hello world</Heading>
   </StrictMode>
 );
